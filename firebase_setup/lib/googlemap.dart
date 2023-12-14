146aa23a4dbbd08951de0598283ddcd32e5edd60
gottenamrathayadav <namrathayadavgotte@gmail.com>
2023-12-13 20:52:03 -0800

    message

---
+++ firebase_setup/lib/googlemap.dart
@@ -0,0 +1,9 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+String userName = "";
+String googleMapKey = "AIzaSyDgh5_Kws-elUHtP1MlXJW80P74KdTUFt8";
+const CameraPosition googlePlexInitialPosition = CameraPosition(
+  target: LatLng(37.42796133580664, -122.085749655962),
+  zoom: 14.4746,
+);
+
---
+++ firebase_setup/lib/home_page.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'googlemap.dart';  // Replace with the correct import
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> googleMapCompleterController =
+  Completer<GoogleMapController>();
+  GoogleMapController? controllerGoogleMap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationEnabled: true,
+            initialCameraPosition: googlePlexInitialPosition,
+            onMapCreated: (GoogleMapController mapController) {
+              controllerGoogleMap = mapController;
+              googleMapCompleterController.complete(controllerGoogleMap);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
